528c00dbfad6a600b5f1f6b2f8199c3dcc43b98a
RaiBnod <binodrayee@gmail.com>
2020-12-01 12:17:18 +0545

    Update script for python3-venv installation

--- script.bash
+++ script.bash
@@ -1,7 +1,6 @@
 sudo apt-get update
-sudo apt-get install build-essential python-dev python-setuptools python3-pip python3-pip virtualenv -y
+sudo apt-get install build-essential python-dev python-setuptools python3-pip python3-pip python3-venv -y
 pip install -U pip setuptools wheel
-rm -r venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
