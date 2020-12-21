bca50c979752fcd1fbc89cec7ed3340c3901b352
RaiBnod <binodrayee@gmail.com>
2020-12-21 14:28:02 +0545

    Improvement on package

--- script.bash
+++ script.bash
@@ -1,6 +1,8 @@
 sudo apt-get update
-sudo apt-get install build-essential python-dev python-setuptools python3-pip python3-pip python3-venv -y
-pip3 install -U pip setuptools wheel
+sudo apt-get install build-essential python3-dev python3-setuptools python3-pip python3-pip python3-venv python3-wheel -y
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade wheel
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
